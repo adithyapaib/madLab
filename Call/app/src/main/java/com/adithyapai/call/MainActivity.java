c83a8ee29f6c84cad38c7ba5ef59d5e4405a219e
Adithya Pai B <paiadithya26@gmail.com>
2022-06-26 15:31:21 +0530

    Initial commit

---
+++ Call/app/src/main/java/com/adithyapai/call/MainActivity.java
@@ -0,0 +1,41 @@
+package com.adithyapai.call;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+public class MainActivity extends AppCompatActivity {
+    EditText phone;
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        phone = findViewById(R.id.phone);
+        findViewById(R.id.call).setOnClickListener(view -> {
+            if (checkSelfPermission(Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED)
+                requestPermissions(new String[]{Manifest.permission.CALL_PHONE}, 1);
+            else
+                startActivity(new Intent(Intent.ACTION_CALL).setData(Uri.parse("tel:" + phone.getText().toString())));
+        });
+        findViewById(R.id.save).setOnClickListener(view -> {
+            startActivity(new Intent(Intent.ACTION_INSERT).setType(ContactsContract.RawContacts.CONTENT_TYPE).putExtra(ContactsContract.Intents.Insert.PHONE, phone.getText().toString()));
+        });
+
+
+    }
+}
---
+++ Text2Speach/app/src/main/java/com/example/text2speach/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.text2speach;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import org.w3c.dom.Text;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        EditText t = findViewById(R.id.text);
+        Button b = findViewById(R.id.button);
+        TextToSpeech tt = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+          @Override
+          public void onInit(int i) {
+              if(i != TextToSpeech.ERROR){
+                  tt.setLanguage(Locale.ENGLISH);
+              }
+
+          }
+      });
+
+       b.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               tt.speak(t.getText().toString(), TextToSpeech.QUEUE_FLUSH, null);
+           }
+       });
+
+
+    }
+}
