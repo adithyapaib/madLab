86b737709a052a1af36762da7823076e24323c0e
Adithya Pai B <paiadithya26@gmail.com>
2022-06-29 21:23:31 +0530

    added new project

---
+++ Login/app/src/main/java/com/adithyapai/login/LoginActivityActivity.java
@@ -0,0 +1,14 @@
+package com.adithyapai.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivityActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activity);
+    }
+}
---
+++ Login/app/src/main/java/com/adithyapai/login/LoginsuccessActivity.java
@@ -0,0 +1,14 @@
+package com.adithyapai.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginsuccessActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.loginsuccess);
+    }
+}
