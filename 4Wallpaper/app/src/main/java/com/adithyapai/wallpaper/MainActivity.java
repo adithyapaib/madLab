25895e9efb9a7c70600f65caec63e0f0d3e6a7e2
Adithya Pai B <paiadithya26@gmail.com>
2022-07-10 12:01:51 +0530

    WallPaper added

---
+++ 4Wallpaper/app/src/main/java/com/adithyapai/wallpaper/MainActivity.java
@@ -0,0 +1,39 @@
+package com.adithyapai.wallpaper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.app.WallpaperManager;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn;
+    WallpaperManager wallpaperManager;
+
+    @SuppressLint("ResourceType")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        wallpaperManager = WallpaperManager.getInstance(this);
+        btn = findViewById(R.id.button);
+        btn.setOnClickListener(v -> {
+            try {
+                wallpaperManager.setResource(R.drawable.b);
+                Thread.sleep(5000);
+                wallpaperManager.setResource(R.drawable.a);
+                Thread.sleep(5000);
+                wallpaperManager.setResource(R.drawable.c);
+
+            } catch (IOException | InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        });
+    }
+}
